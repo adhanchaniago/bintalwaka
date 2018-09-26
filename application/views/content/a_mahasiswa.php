152409377c5fa6b7857c5bbb8ca81616f0a3543b
benben4567 <benben4567@gmail.com>
2018-09-26 11:01:41 +0700

    add filter ui

--- application/views/content/a_mahasiswa.php
+++ application/views/content/a_mahasiswa.php
@@ -1,4 +1,29 @@
-<!-- Tabel User-->
+    <!-- Tabel User-->
+    <div class="row">
+        <div class="col-lg-12 ">
+            <div class="form-group">
+                <label>Tahun Bintalwaka :</label>
+                <select class="form-control">
+                    <option value="">- Semua -</option>
+                    <?php foreach ($tahun as $thn) { ?>
+                        <option value="<?php echo $thn->tahun; ?>"><?php echo $thn->tahun; ?></option>
+                    <?php } ?>
+                </select>
+            </div>
+        </div>
+        <div class="col-lg-12">
+            <div class="form-group">
+                <label>Fakultas :</label>
+                <select class="form-control">
+                    <option value="">- Semua -</option>
+                    <?php foreach ($fakultas as $fak) { ?>
+                        <option value="<?php echo $fak->id_fakultas; ?>"><?php echo $fak->nama_fakultas; ?></option>
+                    <?php } ?>
+                </select>
+            </div>
+        </div>
+    </div>
+    <hr/>
     <table width="100%" class="table table-striped table-bordered table-hover" id="tabel-mahasiswa">
         <thead>
             <tr>
